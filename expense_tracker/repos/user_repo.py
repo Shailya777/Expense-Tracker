8032514f91b6c56f8ccbf6e28bcb8369fa78dfb4
Shailya777 <shailya.gandhi100@gmail.com>
2025-09-12 20:43:48 +0530

    Changes in USer Repository file.

--- expense_tracker/repos/user_repo.py
+++ expense_tracker/repos/user_repo.py
@@ -1,21 +1,81 @@
-from core.db_conn import get_db_connection
+from expense_tracker.core.db_conn import get_db_connection
+from expense_tracker.models.user import User
 
-class UserRepo:
+class UserRepository:
+    """
+    Handles all database operations related to the User model.
+    """
 
-    def __init__(self, db_config):
-        self.conn = get_db_connection(db_config)
+    @staticmethod
+    def create(user: User):
+        """
+        Creates a new user in the database.
 
-    def get_by_id(self, user_id):
-        cursor = self.conn.cursor(dictionary= True)
-        cursor.execute("select user_id, first_name, last_name, email, role from users where user_id = %s", (user_id, ))
-        return cursor.fetchone()
+        :param user: A User object with username, email, and password_hash.
+        :return: User: The created User object with the new database ID.
+        """
 
-    def get_all(self):
-        cursor = self.conn.cursor(dictionary= True)
-        cursor.execute("select user_id, first_name, last_name, email, role from users")
-        return cursor.fetchall()
+        with get_db_connection() as conn:
+            with conn.cursor() as cursor:
+                sql = """
+                    insert into users (username, email, password_hash, role)
+                    values (%s, %s, %s, %s)
+                    """
+                cursor.execute(sql, (user.username, user.email, user.password_hash, user.role))
+                user.id = cursor.lastrowid
+                conn.commit()
+                return user
 
-    def delete(self, user_id):
-        cursor = self.conn.cursor()
-        cursor.execute("delete from users where user_id = %s", (user_id, ))
-        self.conn.commit()
+    @staticmethod
+    def find_by_email(email: str):
+        """
+        Finds a user by their email address.
+
+        :param email: The email address to search for.
+        :return: Optional[User]: A User object if found, otherwise None.
+        """
+
+        with get_db_connection() as conn:
+            with conn.cursor(dictonary = True) as cursor:
+                sql = "select * from users where email = %s"
+                cursor.execute(sql, (email,))
+                row = cursor.fetchone()
+                if row:
+                    return User(**row)
+                return None
+
+    @staticmethod
+    def find_by_username(username: str):
+        """
+        Finds a user by their username.
+
+        :param username: The username to search for.
+        :return: Optional[User]: A User object if found, otherwise None.
+        """
+
+        with get_db_connection() as conn:
+            with conn.cursor(dictionary= True) as cursor:
+                sql = "select * from users where username= %s"
+                cursor.execute(sql, (username,))
+                row = cursor.fetchone()
+                if row:
+                    return User(**row)
+                return None
+
+    @staticmethod
+    def find_by_id(user_id: int):
+        """
+        Finds a user by their unique ID.
+
+        :param user_id: The ID of the user.
+        :return: Optional[User]: A User object if found, otherwise None.
+        """
+
+        with get_db_connection() as conn:
+            with conn.cursor(dictionary= True) as cursor:
+                sql = "select * from users where id = %s"
+                cursor.execute(sql, (user_id,))
+                row = cursor.fetchone()
+                if row:
+                    return User(**row)
+                return None
