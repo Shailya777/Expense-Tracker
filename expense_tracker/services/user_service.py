2ab00058f08d8a51ceedabed167b51d2e9a3e803
Shailya777 <shailya.gandhi100@gmail.com>
2025-09-13 19:50:12 +0530

    Changes in User Service file. Now handles User registration and login.

--- expense_tracker/services/user_service.py
+++ expense_tracker/services/user_service.py
@@ -1,20 +1,62 @@
-from repos.user_repo import UserRepo
-from models.user import User
+import bcrypt
+from typing import Optional
+from expense_tracker.models.user import User
+from expense_tracker.repos.user_repo import UserRepository
 
 class UserService:
+    """
+    Provides business logic for user-related operations like registration and login.
+    """
 
-    def __init__(self, config):
-        self.repo = UserRepo(config)
+    @staticmethod
+    def register(username: str, email: str, password: str):
+        """
+        Registers a new user.
 
-    def view_profile(self, user_id):
-        user_data = self.repo.get_by_id(user_id)
-        return User(**user_data) if user_data else None
+        :param username: The desired username.
+        :param email: The user's email address.
+        :param password: The user's password in plain-text.
 
-    def list_users(self):
-        return self.repo.get_all()
+        :raises
+            ValueError: If the username or email is already taken.
 
-    def delete_user(self, user_id, is_admin):
-        if is_admin:
-            self.repo.delete(user_id)
-            return True
-        return False
+        :return:  User: The newly created user object.
+        """
+
+        if UserRepository.find_by_username(username):
+            raise ValueError(f'Username {username} is already taken.')
+
+        if UserRepository.find_by_email(email):
+            raise ValueError(f'Email {email} is already registered.')
+
+        # Hashing The Password for Secure Storage:
+        hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+        user = User(
+            username= username,
+            email = email,
+            password_hash= hashed_password.decode('utf-8')
+        )
+
+        return UserRepository.create(user)
+
+
+    @staticmethod
+    def login(email: str, password: str):
+        """
+        Authenticates a user based on email and password.
+
+        :param email: The user's email.
+        :param password: The user's password in plain-text.
+
+        :return: Optional[User]: The authenticated User object if credentials are valid,
+                            otherwise None.
+        """
+
+        user = UserRepository.find_by_email(email)
+
+        if user and bcrypt.checkpw(password.encode('utf-8'),
+                                   user.password_hash.encode('utf-8')):
+            return user
+        return None
+
